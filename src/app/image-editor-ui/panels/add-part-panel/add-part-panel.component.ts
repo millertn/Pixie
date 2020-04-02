bc2b13f2766c25448e56daa38b49389577d27b26
Taylor Miller <38132620+millertn@users.noreply.github.com>
2020-04-02 09:09:17 -0400

    Cancel Part Addition
    - Close the panel and delete the placeholder if the user decides to cancel

--- src/app/image-editor-ui/panels/add-part-panel/add-part-panel.component.ts
+++ src/app/image-editor-ui/panels/add-part-panel/add-part-panel.component.ts
@@ -14,6 +14,7 @@ import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import { CanvasService } from 'app/image-editor/canvas/canvas.service';
 import { TextToolService } from 'app/image-editor/tools/text/text-tool.service';
+import { ActiveObjectService } from 'app/image-editor/canvas/active-object/active-object.service';
 
 @Component({
     selector: 'add-part-panel',
@@ -32,5 +33,21 @@ export class AddPartPanelComponent {
         private store: Store,
         private http: HttpClient,
         private textTool: TextToolService,
+        private activeObject:ActiveObjectService,
     ) {}
+
+
+    public cancelAdding() {
+        this.panelRef.close();
+
+        let objects = this.canvas.fabric().getObjects();
+        let placeholder = null;
+        objects.map(object => {
+            if (object.name == "Placeholder Div") {
+                placeholder = object;
+            }
+        });
+        this.canvas.fabric().setActiveObject(placeholder);
+        this.activeObject.delete();
+    }
 }
